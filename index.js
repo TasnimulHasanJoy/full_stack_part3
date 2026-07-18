7c77c2f5bac3c1ab00d5ca1db77d1bfa0fb30a65
TasnimulHasanJoy <mdtasnimulhasanjoy@gmail.com>
2026-07-18 10:39:43 +0300

    Fix frontend serving

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const path = require('path')
 
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('body', (req) => {
@@ -110,7 +112,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
